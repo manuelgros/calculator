a256c7126274e895566572bd611fa4cb2949f14b
manuelgros <131806742+manuelgros@users.noreply.github.com>
2023-08-23 09:49:26 -0700

    Create basic operator functions

---
+++ javascript.js
@@ -0,0 +1,14 @@
+// Function for basic math operators
+
+function add(a, b) {
+  return total = a + b;
+}
+function subtract(a, b) {
+  return total = a - b;
+}
+function multiply(a, b) {
+  return total = a * b;
+}
+function divide(a, b) {
+  return total = a / b;
+}
